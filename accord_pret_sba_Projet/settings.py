5c7896bcea0af778195eab143ab1a20e9446f722
Zouhair <h.mohamed.zouhair@gmail.com>
2023-02-01 16:19:43 +0100

    secret key

--- accord_pret_sba_Projet/settings.py
+++ accord_pret_sba_Projet/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
-load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
+# load_dotenv()
+SECRET_KEY = 'django-insecure-2h4l(0xbvqm#(=d&+wej=a(iwqe1d3o^5@yefw5e(7-0+f042f'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
